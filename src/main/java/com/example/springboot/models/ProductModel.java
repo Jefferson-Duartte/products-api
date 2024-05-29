9a888ee20373bc5bbae86c245f6f020358f2759d
Jefferson Duarte <duartejefferson123@gmail.com>
2024-05-29 00:37:22 -0300

    feat: create Product Model

---
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -0,0 +1,30 @@
+package com.example.springboot.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_PRODUCTS")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProductModel extends RepresentationModel<ProductModel> implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idProduct;
+    private String name;
+    private BigDecimal value;
+
+}
+
